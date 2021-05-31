309d0843ec18bb212049c793e0c704bc13495071
zhangchun <zhangchun5@asiainfo.com>
2021-05-31 16:00:06 +0800

    修改配置

--- bi-task-datax-executer/src/main/java/com/timing/bitaskdataxexecuter/datax/DataxTest.java
+++ bi-task-datax-executer/src/main/java/com/timing/bitaskdataxexecuter/datax/DataxTest.java
@@ -18,20 +18,18 @@ public class DataxTest {
 
         try {
             //datax编译后的目录
-            ClassPathResource resource1 = new ClassPathResource("datax/target/conf/core.json");
-            String path1 = resource1.getFile().getAbsolutePath();
+            String path1 = "/home/bi/BI-TASK/executer/config/datax/target/conf/core.json";
             path1 = path1.substring(0, path1.indexOf("core.json") - 6);
             System.setProperty("datax.home", path1);
             System.setProperty("now", LocalTime.now().toString());
 
             //自己的json文件路径
-            ClassPathResource resource2 = new ClassPathResource("datax/json/mysql2mysql.json");
-            String path2 = resource2.getFile().getAbsolutePath();
+            String path2 = "/home/bi/BI-TASK/executer/config/datax/json/mysql2mysql.json";
             String[] dataxArgs = {"-job", path2, "-mode", "standalone", "-jobid", "-1"};
             Engine.entry(dataxArgs);
         } catch (Throwable throwable) {
-            logger.error("数据同步异常:", throwable.getMessage());
-            XxlJobHelper.log("数据同步异常：", ExceptionTracker.trace(throwable));
+            logger.error("数据同步异常:", throwable);
+            XxlJobHelper.log("数据同步异常：", throwable);
         }
 
         XxlJobHelper.log("数据同步结束！！！");
